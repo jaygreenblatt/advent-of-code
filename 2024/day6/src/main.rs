e3a6c5219f32651e843c46ddc7ba2c131a1c9ba2
jaygreenblatt <jay.greenblatt@gmail.com>
2024-12-08 23:01:14 -0500

    Puzzle 2

--- 2024/day6/src/main.rs
+++ 2024/day6/src/main.rs
@@ -3,6 +3,7 @@ mod track;
 
 fn main() {
     puzzle_one();
+    puzzle_two();
 }
 
 fn puzzle_one() {
@@ -11,4 +12,12 @@ fn puzzle_one() {
 
     println!("Puzzle One Solution:");
     println!("Number of visited positions: {}", num_visited);
+}
+
+fn puzzle_two() {
+    let mut map = parse::parse_file();
+    let num_obstacles = track::get_number_of_obstacles_that_result_in_loop(&mut map);
+
+    println!("Puzzle Two Solution:");
+    println!("Number of obstacles: {}", num_obstacles);
 }
--- 2024/day6/src/track.rs
+++ 2024/day6/src/track.rs
@@ -2,8 +2,9 @@ use std::collections::HashSet;
 
 const GUARD_UP: char = '^';
 const OBSTACLE: char = '#';
+const FREE_SPACE: char = '.';
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 enum Direction {
     LEFT,
     UP,
@@ -66,4 +67,89 @@ fn get_starting_position(map: &Vec<Vec<char>>) -> (i32, i32) {
         }
     }
     coord
+}
+
+
+pub fn get_number_of_obstacles_that_result_in_loop(map: &mut Vec<Vec<char>>) -> i32 {
+    let mut total = 0;
+    let starting_position = get_starting_position(map);
+    let path = get_path(map, starting_position);
+
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            if !path.contains(&(i as i32, j as i32)) {
+                continue;
+            }
+            if map[i][j] == OBSTACLE {
+                continue;
+            }
+            map[i][j] = OBSTACLE;
+
+            if gets_stuck_in_loop(map, starting_position) {
+                total += 1;
+            }
+
+            map[i][j] = FREE_SPACE;
+        }
+    }
+
+    total
+}
+
+
+fn get_path(map: &Vec<Vec<char>>, start: (i32, i32)) -> HashSet<(i32, i32)> {
+    let mut path: HashSet<(i32, i32)> = HashSet::new();
+    let mut coord = start;
+    let mut current_direction = Direction::UP;
+    let m = map.len() as i32;
+    let n = map[0].len() as i32;
+
+    loop {
+        let (x, y) = coord;
+        let (dx, dy) = get_coord_changes(current_direction);
+        path.insert((x, y));
+
+        if x + dx < 0 || x + dx >= m || y + dy < 0 || y + dy >= n {
+            break;
+        }
+        if map[(x + dx) as usize][(y + dy) as usize] == OBSTACLE {
+            current_direction = get_next_direction(current_direction.clone());
+        } else {
+            coord = (x + dx, y + dy)
+        }
+    }
+
+    path
+}
+
+
+fn gets_stuck_in_loop(map: &Vec<Vec<char>>, start: (i32, i32)) -> bool {
+    
+    let mut coord = start;
+    let mut current_direction = Direction::UP;
+    let mut visited: HashSet<(i32, i32, Direction)> = HashSet::new();
+    let m = map.len() as i32;
+    let n = map[0].len() as i32;
+
+    loop {
+        let (x, y) = coord;
+
+        if visited.contains(&(x, y, current_direction)) {
+            return true;
+        }
+
+        let (dx, dy) = get_coord_changes(current_direction);
+        visited.insert((x, y, current_direction));
+
+        if x + dx < 0 || x + dx >= m || y + dy < 0 || y + dy >= n {
+            break;
+        }
+        if map[(x + dx) as usize][(y + dy) as usize] == OBSTACLE {
+            current_direction = get_next_direction(current_direction.clone());
+        } else {
+            coord = (x + dx, y + dy)
+        }
+    }
+
+    false
 }
