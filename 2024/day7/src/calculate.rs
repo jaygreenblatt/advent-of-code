a72279113bfd7660ff9c718d6408d0dc68dc093f
jaygreenblatt <jay.greenblatt@gmail.com>
2024-12-13 23:15:28 -0500

    Puzzle one

--- 2024/day7/src/calculate.rs
+++ 2024/day7/src/calculate.rs
@@ -15,3 +15,28 @@ pub fn calibrate(total: i64, curr_total: i64, i: usize, nums: &Vec<i64>) -> i64 
 
     max(add_result, mul_result)
 }
+
+
+pub fn calibrate_v2(total: i64, curr_total: i64, i: usize, nums: &Vec<i64>) -> i64 {
+
+    if i == nums.len() && curr_total == total {
+        return curr_total;
+    }
+
+    if i == nums.len() {
+        return 0;
+    }
+    
+    let add_result = calibrate(total, curr_total + nums[i], i + 1, nums);
+    let mul_result = calibrate(total, curr_total * nums[i], i + 1, nums);
+
+    let concatenated = concatenate_numbers(curr_total, nums[i]);
+    let concat_result = calibrate(total, concatenated, i + 1, nums);
+
+    max(max(add_result, mul_result), concat_result)
+}
+
+
+fn concatenate_numbers(a: i64, b: i64) -> i64 {
+    format!("{}{}", a, b).parse().unwrap()
+}
--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -3,6 +3,7 @@ mod calculate;
 
 fn main() {
     puzzle_one();
+    puzzle_two();
 }
 
 fn puzzle_one() {
@@ -13,5 +14,17 @@ fn puzzle_one() {
     }
 
     println!("Puzzle One Solution:");
+    println!("The total is {}", total);
+}
+
+
+fn puzzle_two() {
+    let calibrations = parse::parse_file();
+    let mut total = 0;
+    for calibration in calibrations.iter() {
+        total += calculate::calibrate_v2(calibration.0, calibration.1[0], 1, &calibration.1);
+    }
+
+    println!("Puzzle Two Solution:");
     println!("The total is {}", total);
 }
